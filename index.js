802e41db6ac8abede45f0be545c2e10715ec2682
Pietro Moro <pietromoro@programmer.net>
2020-02-25 16:38:00 +0100

    Add index.html

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { app, BrowserWindow } = require('electron')
 let window;
 function createWindow() {
   window = new BrowserWindow({ width: 800, height: 600 });
+  window.loadFile('index.html');
   window.webContents.openDevTools();
 
   window.on('closed', () => window = null);
