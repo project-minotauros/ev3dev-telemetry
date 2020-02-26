477a888e36598ee34db5c92feb2c1a1cb1335d6f
Pietro Moro <pietromoro@programmer.net>
2020-02-26 17:29:23 +0100

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    modules: ["node_modules"],
+    extensions: ['.js', '.ts', '.tsx']
+  },
+};
