0e82889a6144fc3c2696b46825207b257cabe604
Pietro Moro <pietromoro@programmer.net>
2020-04-16 23:13:11 +0200

    Add FPS counter

--- src/debug.ts
+++ src/debug.ts
@@ -6,6 +6,7 @@ export function debug_window(clear_color : any, mesh : any) {
 	ImGui.Begin("Debug");
 	ImGui.ColorEdit3("clear color", clear_color);
 	ImGui.Text(`Mesh x rotation: ${mesh.rotation.x.toString()}`);
+  ImGui.Text(`Application average ${(1000.0 / ImGui.GetIO().Framerate).toFixed(3)} ms/frame\n(${ImGui.GetIO().Framerate.toFixed(1)} FPS)`);
 	//ImGui.SliderFloat3("scale", mesh.scale, -2, 2);
 	ImGui.Checkbox("visible", (value = mesh.visible) => mesh.visible = value);
 	ImGui.End();
