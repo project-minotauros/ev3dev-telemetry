f00d5796d20a161b25392ba51d3cad9f372607ad
Pietro Moro <pietromoro@programmer.net>
2020-05-14 13:05:04 +0200

    Move socket object to state

--- src/initial_configuration.ts
+++ src/initial_configuration.ts
@@ -6,7 +6,7 @@ let error: boolean = false;
 let error_text: string = "";
 let connecting: boolean = false;
 
-export function initialize_popup(state: any, socket: any) {
+export function initialize_popup(state: any) {
   if (!(state.ready)) ImGui.OpenPopup("Enter ev3 address");
   if (ImGui.BeginPopupModal("Enter ev3 address", null, ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoMove)) {
     if (connecting && !error) {
@@ -17,7 +17,7 @@ export function initialize_popup(state: any, socket: any) {
       ImGui.Text("Please enter the local address of your ev3 brick...");
       ImGui.InputText("ev3 address", ev3_address.value, IM_ARRAYSIZE(ev3_address.value));
       if (ImGui.Button("Connect"))
-        connect(socket, ev3_address.value.buffer, state);
+        connect(ev3_address.value.buffer, state);
       if (error)
         ImGui.Text("[Error while connecting]: " + error_text);
       ImGui.EndPopup();
@@ -25,17 +25,17 @@ export function initialize_popup(state: any, socket: any) {
   }
 }
 
-function connect(socket: any, ev3_address: string, state: any) {
+function connect(ev3_address: string, state: any) {
   connecting = true;
-  socket = new WebSocket(`ws://${ev3_address}:4567`);
-  socket.onerror = (event: any) => {
+  state.socket = new WebSocket(`ws://${ev3_address}:4567`);
+  state.socket.onerror = (event: any) => {
     error_text = event.toString();
     error = true;
   };
-  socket.onclose = (event: any) => {
+  state.socket.onclose = (event: any) => {
     state.ready = false;
   };
-  socket.onopen = (event: any) => {
+  state.socket.onopen = (event: any) => {
     state.ready = true;
     error = false;
     error_text = "";
