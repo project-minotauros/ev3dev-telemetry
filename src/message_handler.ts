aef53ca8ac09a05bf9f834ac70b7fe9201f643b7
Pietro Moro <pietromoro@programmer.net>
2020-05-15 23:45:53 +0200

    Message handling baseline

--- src/message_handler.ts
+++ src/message_handler.ts
@@ -1,9 +1,25 @@
 import Marshal from './marshal';
+import {InboundFlags, AvailableDevices} from './message_type';
 
 export function handle_message(state: any, cpanel_state: any) {
   if (state.ready && state.socket.onmessage == null) state.socket.onmessage = (message: any) => {
     console.log(message, cpanel_state);
-    let header = message.data[0];
-    console.log(header, new Marshal(message.data.slice(1), 'utf8'));
+    let header = message.data[0].charCodeAt(0);
+    let response_type = header >> 7;
+    let device_type = (header >> 3) & 15;
+    let device_id = (header >> 0) & 7;
+    let payload = new Marshal(message.data.slice(1), 'utf8').parsed;
+    switch (response_type) {
+      case InboundFlags.CONSOLE_OUTPUT:
+        break;
+      case InboundFlags.CONSOLE_ERROR:
+        break;
+      case InboundFlags.AVAILABLE_DEVICES:
+        break;
+      case InboundFlags.UPDATE_INFO:
+        break;
+      default:
+        throw new Error('Response type not recognized.');
+    }
   };
 }
