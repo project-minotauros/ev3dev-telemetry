33b21a38870653bc7b2e21075dbc5f4d599e87b3
Pietro Moro <pietromoro@programmer.net>
2020-05-15 15:59:55 +0200

    Marshal example load

--- src/message_handler.ts
+++ src/message_handler.ts
@@ -1,5 +1,9 @@
+import Marshal from './marshal';
+
 export function handle_message(state: any, cpanel_state: any) {
-  if (state.ready && state.socket.onmessage == null) state.socket.onmessage = (message: string) => {
+  if (state.ready && state.socket.onmessage == null) state.socket.onmessage = (message: any) => {
     console.log(message, cpanel_state);
+    let header = message.data[0];
+    console.log(header, new Marshal(message.data.slice(1), 'utf8'));
   };
 }
