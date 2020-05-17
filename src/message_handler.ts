b516325ea7832db31ff872a0bad9b3bb602c097f
Pietro Moro <pietromoro@programmer.net>
2020-05-17 22:02:06 +0200

    Handle available devices and update info

--- src/message_handler.ts
+++ src/message_handler.ts
@@ -1,15 +1,15 @@
 import Marshal from './marshal';
-import {InboundFlags, OutboundFlags, AvailableDevices} from './message_type';
+import {InboundFlags, OutboundFlags, AvailableDevices, GetDeviceName} from './message_type';
 import {logToConsole} from './basic_panels';
 
 export function handle_message(state: any, cpanel_state: any) {
   if (state.ready && state.socket.onmessage == null) state.socket.onmessage = (message: any) => {
-    console.log(message, cpanel_state);
     let header = parseInt(message.data.slice(0, message.data.indexOf('P')));
     let response_type = header >> 7;
     let device_type = (header >> 3) & 15;
     let device_id = (header >> 0) & 7;
     let payload = new Marshal(message.data.slice(message.data.indexOf('P') + 1), 'utf8').parsed;
+    console.log(header, payload);
     switch (response_type) {
       case InboundFlags.CONSOLE_OUTPUT:
         logToConsole(payload);
@@ -18,8 +18,10 @@ export function handle_message(state: any, cpanel_state: any) {
         logToConsole(payload, true);
         break;
       case InboundFlags.AVAILABLE_DEVICES:
+        save_available_devices(state, device_type, payload);
         break;
       case InboundFlags.UPDATE_INFO:
+        Object.assign(state.devices[GetDeviceName(device_type)], payload);
         break;
       default:
         throw new Error('Response type not recognized.');
@@ -45,4 +47,9 @@ export function encode_command(command: number, sub_command: number, device_type
 
 export function send_command(state: any, command: string, payload: string = "") {
   state.socket.send(command + payload);
+}
+
+function save_available_devices(state: any, device_type: any, payload: any) {
+  // TODO handle different device types
+  state.devices = payload;
 }
