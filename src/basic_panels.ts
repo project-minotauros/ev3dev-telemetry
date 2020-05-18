103cb114c46781519b66fb0ee30d4b93fd5168d8
Pietro Moro <pietromoro@programmer.net>
2020-05-18 10:34:55 +0200

    Add battery panel

--- src/basic_panels.ts
+++ src/basic_panels.ts
@@ -9,6 +9,9 @@ export function display_basic_panels(state: any, cpanel_state: any) {
 
   if (cpanel_state.console)
     console_panel(state, cpanel_state);
+
+  if (cpanel_state.battery_info)
+    battery_panel(state, cpanel_state);
 }
 
 var commandLine: ImStringBuffer = new ImStringBuffer(256, "");
@@ -77,4 +80,38 @@ export function logToConsole(message: string, error: boolean = false) {
   if (error) message = "[ERROR] " + message;
   consoleHistory.push_back(message);
   if (autoScroll) scrollToBottom = true;
+}
+
+const current_voltages: Static<number[]> = STATIC("voltages_a", [0]);
+const current_currents: Static<number[]> = STATIC("currents_a", [0]);
+function battery_panel(state: any, cpanel_state: any) {
+  if (!ImGui.Begin("Battery Info", (v = cpanel_state.battery_info) => cpanel_state.battery_info = v)) {
+    ImGui.End();
+    return;
+  }
+
+  if (state.devices.battery["current_now"]) {
+    let current = parseInt(state.devices.battery["current_now"]);
+    if (current_currents.value.length > 90) current_currents.value.shift();
+    current_currents.value.push(current);
+  }
+  if (state.devices.battery["voltage_now"]) {
+    let voltage = parseInt(state.devices.battery["voltage_now"]);
+    if (current_voltages.value.length > 90) current_voltages.value.shift();
+    current_voltages.value.push(voltage);
+  }
+
+  ImGui.Text("Type:"); ImGui.SameLine(); ImGui.Text(state.devices.battery.type);
+  ImGui.Text("Scope:"); ImGui.SameLine(); ImGui.Text(state.devices.battery.scope);
+  ImGui.Text("Technology:"); ImGui.SameLine(); ImGui.Text(state.devices.battery.technology);
+  ImGui.Text("");
+  ImGui.Text("Minimum Voltage:"); ImGui.SameLine(); ImGui.Text(state.devices.battery.voltage_min_design);
+  ImGui.Text("Maximum Voltage:"); ImGui.SameLine(); ImGui.Text(state.devices.battery.voltage_max_design);
+  ImGui.Text("");
+  ImGui.Text("Current now:"); ImGui.SameLine(); ImGui.Text(state.devices.battery["current_now"] || "");
+  ImGui.PlotLines("Current", current_currents.value, IM_ARRAYSIZE(current_currents.value));
+  ImGui.Text("Voltage now:"); ImGui.SameLine(); ImGui.Text(state.devices.battery["voltage_now"] || "");
+  ImGui.PlotLines("Voltage", current_voltages.value, IM_ARRAYSIZE(current_voltages.value));
+
+  ImGui.End();
 }
